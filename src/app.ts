abe7a2a3f4b754ad723ebe815d10bfdd57208cbf
educcarnn <educcarnn@gmail.com>
2023-07-24 20:16:45 -0300

    fix: package json

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,20 @@
-const express = require('express')
+const express = require("express");
+const helmet = require("helmet");
 
-import purchaseListRoutes from './routes/purchaseListRoutes';
+import purchaseListRoutes from "./routes/purchaseListRoutes";
 
 const app = express();
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self'"],
+      fontSrc: ["'self'", "https://fonts.googleapis.com"],
+    },
+  })
+);
 
 app.use(express.json());
 
-app.use('/api', purchaseListRoutes);
-
-
+app.use("/api", purchaseListRoutes);
 
 export default app;
