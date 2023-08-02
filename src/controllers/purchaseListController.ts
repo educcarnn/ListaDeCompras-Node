380610e71da037f228854013ae38b73813899da8
educcarnn <educcarnn@gmail.com>
2023-08-01 21:44:52 -0300

    feat: implement delete item by list specific

--- src/controllers/purchaseListController.ts
+++ src/controllers/purchaseListController.ts
@@ -25,7 +25,7 @@ export const getAllPurchaseLists = (req: Request, res: Response) => {
     return res.status(200).json(purchaseLists);
   };
 
-  export const getPurchaseListById = (req: Request, res: Response) => {
+export const getPurchaseListById = (req: Request, res: Response) => {
     const id = parseInt(req.params.purchaseListId, 10);
     const list = purchaseLists.find((item) => item.id === id);
   
@@ -34,34 +34,34 @@ export const getAllPurchaseLists = (req: Request, res: Response) => {
     }
   
     return res.status(200).json(list);
-  };
+};
 
-  export const updateListItem = (req: Request, res: Response) => {
-    const id = parseInt(req.params.purchaseListId, 10);
-    const itemName = req.params.itemName;
-    const newItemValue = req.body.value;
-  
-    const list = purchaseLists.find((item) => item.id === id);
-  
-    if (!list) {
-      return res.status(404).json({ message: 'Lista não encontrada' });
-    }
-  
-    const itemToUpdate = list.items.find((item) => item.name === itemName);
-  
-    if (!itemToUpdate) {
-      return res.status(404).json({ message: 'Item não encontrado na lista' });
-    }
-  
-    if (typeof newItemValue !== 'number') {
-      return res.status(400).json({ message: 'Tipo do valor de entrada inválido' });
-    }
-  
-    itemToUpdate.value = newItemValue;
-    return res.status(200).json(list);
-  };
+export const updateListItem = (req: Request, res: Response) => {
+  const id = parseInt(req.params.purchaseListId, 10);
+  const itemName = req.params.itemName;
+  const newItemValue = req.body.value;
 
-  export const deleteListItem = (req: Request, res: Response) => {
+  const list = purchaseLists.find((item) => item.id === id);
+
+  if (!list) {
+    return res.status(404).json({ message: 'Lista não encontrada' });
+  }
+
+  const itemToUpdate = list.items.find((item) => item.name === itemName);
+
+  if (!itemToUpdate) {
+    return res.status(404).json({ message: 'Item não encontrado na lista' });
+  }
+
+  if (typeof newItemValue !== 'number') { // Correção aqui
+    return res.status(400).json({ message: 'Tipo do valor de entrada inválido' });
+  }
+
+  itemToUpdate.value = newItemValue;
+  return res.status(200).json(list);
+};
+
+export const deleteListItem = (req: Request, res: Response) => {
     const id = parseInt(req.params.purchaseListId, 10);
     const itemName = req.params.itemName;
   
--- src/routes/purchaseListRoutes.ts
+++ src/routes/purchaseListRoutes.ts
@@ -4,6 +4,7 @@ import {
     createPurchaseList,
     getAllPurchaseLists,
     getPurchaseListById,
+    deleteListItem,
 } from '../controllers/purchaseListController';
 
 const router = express.Router();
@@ -11,6 +12,8 @@ const router = express.Router();
 router.post('/purchaseList', createPurchaseList);
 router.get('/purchaseList', getAllPurchaseLists);
 router.get('/purchaseList/:purchaseListId', getPurchaseListById);
-router.get('/purchaseList/:purchaseListId', getPurchaseListById)
+
+router.delete('/purchaseList/:purchaseListId/:itemName', deleteListItem)
+
 
 export default router;
